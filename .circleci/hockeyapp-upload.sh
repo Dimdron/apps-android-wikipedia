8e451536445dbe716170e5f00e0be35a0e0bbc99
pavelfedor <pavelbjorn@gmail.com>
2018-07-02 15:09:36 +0300

    Fix repository order to make project build

--- .circleci/hockeyapp-upload.sh
+++ .circleci/hockeyapp-upload.sh
@@ -2,7 +2,7 @@ curl \
   -F "status=2" \
   -F "notes=$RELEASE_NOTES" \
   -F "notes_type=1" \
-  -F "ipa=@app/build/outputs/apk/dev/debug/app-dev-debug.apk" \
+  -F "ipa=@$APK_PATH" \
   -F "notify=1" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
   https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload
